89378cd20eb3241f218f9f34ec8157cd00b9a3db
discopatrick <git@discopatrick.me>
2017-03-09 11:47:11 +0000

    moves artwork loader into separate js file

--- js/lib.js
+++ js/lib.js
@@ -1,37 +1,3 @@
-$(document).ready(function(){
-
-  // show the artwork specified in the querystring
-  script = getParameterByName('show');
-  $.getScript('./js/three/' + script + '.js');
-
-  // populate the title tag
-  $("title").text(script);
-
-  // populate and update the dropdown
-  $.ajax({
-      url : "https://api.github.com/repos/discopatrick/digitalart/contents/js/three",
-      success: function(artworks) {
-        for (var i = 0; i < artworks.length; i++) {
-          var option = artworks[i].name.substring(0, artworks[i].name.indexOf('.js'));
-          $('#artworks').append($('<option>', {
-            value: option,
-            text: option
-          }));
-        }
-        $('#artworks > option[value="' + script + '"]').prop('selected', true);
-      },
-      async: true
-  });
-
-  // load a new artwork upon dropdown change
-  $('#artworks').change(function(){
-      var artwork = $('#artworks option:selected').val();
-      console.log(artwork);
-      window.location.href = './?show=' + artwork;
-  });
-
-});
-
 function getParameterByName(name, url) {
     if (!url) {
       url = window.location.href;
