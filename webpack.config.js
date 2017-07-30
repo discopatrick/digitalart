917aee0bdf8b6824ab8b3a14584bf8ae5e55747c
discopatrick <git@discopatrick.me>
2017-07-30 20:08:55 +0100

    Update publicPath setting to fix dev server browser autoreload

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
   ],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/'
   },
   devServer: {
     compress: true,
