fbd54c8ee8d9c60e66e12e6a3837792fcf0e3ff6
discopatrick <git@discopatrick.me>
2017-07-30 20:14:30 +0100

    Install three and add example to index.js

--- app/index.js
+++ app/index.js
@@ -1,9 +1,38 @@
-function component () {
-  var element = document.createElement('div');
+var THREE = require('three');
 
-  element.innerHTML = 'Hello webpack';
+var scene, camera, renderer;
+var geometry, material, mesh;
 
-  return element;
+init();
+animate();
+
+function init() {
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.z = 1000;
+
+    geometry = new THREE.BoxGeometry( 200, 200, 200 );
+    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    document.body.appendChild( renderer.domElement );
+
 }
 
-document.body.appendChild(component());
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render( scene, camera );
+
+}
