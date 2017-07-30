fa6b57d8a51a008762fb6c103ff51ff95b024517
discopatrick <git@discopatrick.me>
2017-07-30 17:48:06 +0100

    Install and configure babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 var path = require('path');
 
 module.exports = {
-  entry: './app/index.js',
+  entry: [
+    // Set up an ES6-ish environment
+    'babel-polyfill',
+
+    // Add your application's scripts below
+    './app/index.js',
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
@@ -9,5 +15,25 @@ module.exports = {
   devServer: {
     compress: true,
     port: 9000
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+
+        // Skip any files outside of your project's `app` directory
+        include: [
+          path.resolve(__dirname, "app"),
+        ],
+
+        // Only run `.js` and `.jsx` files through Babel
+        test: /\.jsx?$/,
+
+        // Options to configure babel with
+        query: {
+          plugins: ['transform-runtime']
+        }
+      }
+    ]
   }
 };
