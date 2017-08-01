9e2cc9663c63a48888471c0b7c311cc4ec7dd83e
discopatrick <git@discopatrick.me>
2017-08-01 13:44:49 +0100

    Convert ThreeWrapper to ES6

---
+++ app/lib/wrapper.js
@@ -0,0 +1,25 @@
+var THREE = require('three');
+
+class ThreeWrapper {
+  constructor() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera.position.z = 25;
+    this.renderer = new THREE.WebGLRenderer();
+
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+  }
+
+  render() {
+    // leave empty, can be defined later
+  }
+
+  animate() {
+    requestAnimationFrame(this.animate);
+    this.render();
+    this.renderer.render(this.scene, this.camera);
+  }
+}
+
+export { ThreeWrapper };
