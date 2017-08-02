d6e0daea7846aafa24a5b2492417cbb5ad56ba99
discopatrick <git@discopatrick.me>
2017-08-02 15:16:53 +0100

    Load animation based on querystring

--- app/index.js
+++ app/index.js
@@ -3,5 +3,28 @@ import { ThreeWrapper } from './lib/wrapper'
 import { SpinningBox } from './artworks/spinning-box.js'
 import { CurvyWorm } from './artworks/curvy-worm.js'
 
-var animation = new CurvyWorm()
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var classMap = {
+   SpinningBox: SpinningBox,
+   CurvyWorm: CurvyWorm
+};
+
+var artwork = getParameterByName('show');
+
+var className = artwork.replace(/(\-|^)([a-z])/gi, function (match, delimiter, hyphenated) {
+    return hyphenated.toUpperCase();
+});
+
+var animation = new classMap[className]()
 animation.start()
